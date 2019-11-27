a81f956bb7df18885ce88015f447235ef7485238
rko1985 <35703264+rko1985@users.noreply.github.com>
2019-11-27 12:36:30 -1000

    album photo listing

--- resources/views/albums/show.blade.php
+++ resources/views/albums/show.blade.php
@@ -13,4 +13,31 @@
     </div>
 </section>
 
+<div class="container">
+    @if( count($album->photos) > 0)
+
+        <div class="row">
+            @foreach($album->photos as $photo)
+                <div class="col-md-4">
+                    <div class="card mb-4 box-shadow">
+                    <img src="/storage/albums/{{$album->id}}/{{$photo->photo}}" alt="{{$photo->photo}}" height="200px">
+                    <div class="card-body">
+                        <p class="card-text">{{$photo->description}}</p>
+                        <div class="d-flex justify-content-between align-items-center">
+                        <div class="btn-group">
+                            <a href="{{ route('photo-show', $photo->id) }}" class="btn btn-sm btn-outline-secondary">View</a>
+                        </div>
+                        <small class="text-muted">{{$photo->size}}</small>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    
+    @else
+        <h3>No albums yet.</h3>
+    @endif
+</div>
+
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,4 @@ Route::get('albums/{id}', 'AlbumsController@show')->name('album-show');
 
 Route::get('/photos/create/{albumId}', 'PhotosController@create')->name('photo-create');
 Route::post('/photos/store', 'PhotosController@store')->name('photo-store');
+Route::get('/photos/{id}', 'PhotosController@show')->name('photo-show');
